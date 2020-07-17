dfe620db8a51875e8be770f83de6033e12d812c6
Thiago Vieira <thiiagovieir@gmail.com>
2020-07-17 17:20:20 -0300

    fix - configurando xdebug

--- containers/php-docker/public/install.sh
+++ containers/php-docker/public/install.sh
@@ -16,3 +16,11 @@ mv composer.phar /usr/local/bin/composer;
 # Install Symfony CLI
 wget https://get.symfony.com/cli/installer -O - | bash;
 mv /root/.symfony/bin/symfony /usr/local/bin/symfony;
+
+# Configurando XDebug
+echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+&& echo "xdebug.remote_handler=dbgp" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+&& echo "xdebug.remote_port=9000" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+&& echo "xdebug.remote_autostart=on" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+&& echo "xdebug.remote_connect_back=0" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+&& echo "xdebug.idekey=docker" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
