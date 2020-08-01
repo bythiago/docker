ad7b147624a88fc2a8c28ccd7fa2c4d592ade443
Thiago Vieira <thiiagovieir@gmail.com>
2020-08-01 11:39:42 -0300

    fix - php5.6

--- containers/php-5.6/public/install.sh
+++ containers/php-5.6/public/install.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
 if [ ! -f "/etc/install.lock" ]; then
-    # Install Packages
-    apt-get update;
-    apt-get upgrade -y;
-    apt-get intall ruby-full;
 
-    # Install Pdo oci
-    tar xjf php-5.5.9.tar.bz2
-    cd php-5.5.9/ext/pdo_oci
-    phpize
-
-    # Install Composer
+    #
     curl -s https://getcomposer.org/installer | php;
     mv composer.phar /usr/local/bin/composer;
-
-    # Configure Git
+    
     git config --global user.email "thiiagovieir@gmail.com"
     git config --global user.name "Thiago Vieira"
 
