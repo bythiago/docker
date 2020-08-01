0010465582e39d569cd51d49fd6713dd3c008277
Thiago Vieira <thiiagovieir@gmail.com>
2020-07-31 23:42:49 -0300

    hotfix

--- containers/php-5.6/public/install.sh
+++ containers/php-5.6/public/install.sh
@@ -4,31 +4,20 @@ if [ ! -f "/etc/install.lock" ]; then
     # Install Packages
     apt-get update;
     apt-get upgrade -y;
-    apt-get install -y git libzip-dev wget;
+    apt-get intall ruby-full;
 
-    pecl install xdebug;
-    pecl install zip;
-    docker-php-ext-enable xdebug;
-    docker-php-ext-enable zip;
+    # Install Pdo oci
+    tar xjf php-5.5.9.tar.bz2
+    cd php-5.5.9/ext/pdo_oci
+    phpize
 
     # Install Composer
     curl -s https://getcomposer.org/installer | php;
     mv composer.phar /usr/local/bin/composer;
 
-    # Configurando XDebug
-    echo "xdebug.default_enable=on" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
-    && echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
-    && echo "xdebug.remote_handler=dbgp" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
-    && echo "xdebug.remote_mode=req" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
-    && echo "xdebug.remote_port=9000" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
-    && echo "xdebug.remote_autostart=on" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
-    && echo "xdebug.remote_connect_back=on" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
-    && echo "xdebug.idekey=VSCODE" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
-    && echo "xdebug.remote_host=host.docker.internal" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
-
-    # Configurando GIT
-    git config --global user.email "email@email.com"
-    git config --global user.name "username"
+    # Configure Git
+    git config --global user.email "thiiagovieir@gmail.com"
+    git config --global user.name "Thiago Vieira"
 
     touch /etc/install.lock
 fi
