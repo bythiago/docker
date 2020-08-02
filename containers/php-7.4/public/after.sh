05efe74912f7bfdeb9f8d8933e9fc7024a9b9535
Thiago Vieira <thiiagovieir@gmail.com>
2020-08-01 22:02:19 -0300

    fix - php7.4 with oci8

--- containers/php-7.4/public/after.sh
+++ containers/php-7.4/public/after.sh
@@ -5,12 +5,7 @@ if [ ! -f "/etc/install.lock" ]; then
     apt-get update
     apt-get install -y alien wget
 
-    # update-alternatives --set php /usr/bin/php7.4
-    # update-alternatives --set php-config /usr/bin/php-config7.4
-    # update-alternatives --set phpize /usr/bin/phpize7.4
-
-    mkdir downloads
-    cd downloads
+    cd /tmp
 
     echo 'Downloading oracle instant client basic'
     wget -nv https://www.dropbox.com/s/q7fkj3d1hn2scrl/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm
@@ -46,31 +41,4 @@ if [ ! -f "/etc/install.lock" ]; then
     service php7.4-fpm restart
 
     touch /etc/install.lock
-else
-    echo "Script já executado"
 fi
-
-#If you would like to do some extra provisioning you may
-# add any commands you wish to this file and they will
-# be run after the Homestead machine is provisioned.
-#
-# If you have user-specific configurations you would like
-# to apply, you may also create user-customizations.sh,
-# which will be run after this script.
-
-# If you're not quite ready for Node 12.x
-# Uncomment these lines to roll back to
-# v11.x or v10.x
-
-# Remove Node.js v12.x:
-#apt-get -y purge nodejs
-#rm -rf /usr/lib/node_modules/npm/lib
-#rm -rf //etc/apt/sources.list.d/nodesource.list
-
-# Install Node.js v11.x
-#curl -sL https://deb.nodesource.com/setup_11.x | -E bash -
-#apt-get install -y nodejs
-
-# Install Node.js v10.x
-#curl -sL https://deb.nodesource.com/setup_10.x | -E bash -
-#apt-get install -y nodejs
