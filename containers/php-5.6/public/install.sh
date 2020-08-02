5620b83b8a1fdfae7ddee9eacb2469acd0c9c7d5
Thiago Vieira <thiiagovieir@gmail.com>
2020-08-02 10:59:07 -0300

    hotfix - php5.6

--- containers/php-5.6/public/install.sh
+++ containers/php-5.6/public/install.sh
@@ -10,6 +10,7 @@ if [ ! -f "/etc/install.lock" ]; then
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB;
     curl -sSL https://get.rvm.io | bash -s stable;
     source /usr/local/rvm/scripts/rvm;
+    echo "source /usr/local/rvm/scripts/rvm;" >> /root/.bashrc
     rvm install 2.7.1;
     rvm use 2.7.1 --default;
     ruby -v;
